58b567cb02951df2346738af98e214f2314da36a
takuya-fukumura <blackchoco1114@gmail.com>
2025-08-27 22:10:17 +0900

    kspを使うように変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.kapt)
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.ksp)
 }
 
 android {
